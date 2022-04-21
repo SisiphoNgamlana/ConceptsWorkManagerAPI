f131a303142688ee7fc6580ca2437b6b4528dc64
Amandla Ngamlana <amandlangamlana@yahoo.com>
2022-04-21 20:56:03 +0200

    Link workers with activity

--- app/src/main/java/com/example/conceptsworkmanagerapi/CreateCardActivity.java
+++ app/src/main/java/com/example/conceptsworkmanagerapi/CreateCardActivity.java
@@ -1,10 +1,17 @@
 package com.example.conceptsworkmanagerapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.work.Data;
+import androidx.work.WorkInfo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,10 +19,16 @@ import android.widget.ProgressBar;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
+import static com.example.conceptsworkmanagerapi.Constants.KEY_IMAGE_URI;
+
 public class CreateCardActivity extends AppCompatActivity {
 
+    public static final String TAG = CreateCardActivity.class.getName();
+
     private CustomCardViewModel customCardViewModel;
-    private Button processCard, cancelCard, seeCardButton;
+    private Button processCard, cancelProcess, seeCardButton;
     private ImageView imageView;
     private EditText quoteEditText;
     private ProgressBar progressBar;
@@ -26,25 +39,94 @@ public class CreateCardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_card);
 
 
-
         imageView = findViewById(R.id.image_view);
         quoteEditText = findViewById(R.id.custom_quote_edtx);
-        seeCardButton =  findViewById(R.id.see_card_button);
-        cancelCard =  findViewById(R.id.cancel_button);
-        processCard =  findViewById(R.id.process_button);
-        progressBar =  findViewById(R.id.progress_bar);
+        seeCardButton = findViewById(R.id.see_card_button);
+        cancelProcess = findViewById(R.id.cancel_button);
+        processCard = findViewById(R.id.process_button);
+        progressBar = findViewById(R.id.progress_bar);
 
         customCardViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication())
                 .create(CustomCardViewModel.class);
 
         Intent intent = getIntent();
-        String imageUriExtra = intent.getStringExtra(Constants.KEY_IMAGE_URI);
+        String imageUriExtra = intent.getStringExtra(KEY_IMAGE_URI);
         customCardViewModel.setImageUri(imageUriExtra);
 
-        if(customCardViewModel.getImageUri() != null){
+        if (customCardViewModel.getImageUri() != null) {
             Picasso.get()
                     .load(customCardViewModel.getImageUri())
                     .into(imageView);
         }
+
+        customCardViewModel.getOutputWorkInfo().observe(this, new Observer<List<WorkInfo>>() {
+            @Override
+            public void onChanged(List<WorkInfo> workInfos) {
+                if (workInfos == null || workInfos.isEmpty()) {
+                    return;
+                }
+                WorkInfo workInfo = workInfos.get(0);
+                boolean finished = workInfo.getState().isFinished();
+                if (!finished) {
+                    showWorkInProgress();
+
+                } else {
+                    showWorkFinished();
+                    Data outputData = workInfo.getOutputData();
+                    String outputImageUri = outputData.getString(KEY_IMAGE_URI);
+
+                    if (!TextUtils.isEmpty(outputImageUri)) {
+                        customCardViewModel.setOutputUri(outputImageUri);
+                        seeCardButton.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        processCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!TextUtils.isEmpty(quoteEditText.getText().toString())) {
+                    String quote = quoteEditText.getText().toString();
+                    customCardViewModel.processImageToCard(quote);
+                }
+            }
+        });
+
+        seeCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "OnClick: See card");
+                Uri currentUri = customCardViewModel.getOutUri();
+                if (currentUri != null) {
+                    Intent actionView = new Intent(Intent.ACTION_VIEW, currentUri);
+                    if (actionView.resolveActivity(getPackageManager()) != null) {
+                        startActivity(actionView);
+                    }
+                    processCard.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        cancelProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customCardViewModel.cancelWork();
+            }
+        });
     }
+
+
+    private void showWorkFinished() {
+        progressBar.setVisibility(View.GONE);
+        cancelProcess.setVisibility(View.GONE);
+        seeCardButton.setVisibility(View.VISIBLE);
+    }
+
+    private void showWorkInProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        cancelProcess.setVisibility(View.VISIBLE);
+        seeCardButton.setVisibility(View.GONE);
+    }
+
 }
--- app/src/main/java/com/example/conceptsworkmanagerapi/CustomCardViewModel.java
+++ app/src/main/java/com/example/conceptsworkmanagerapi/CustomCardViewModel.java
@@ -4,18 +4,26 @@ import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.example.conceptsworkmanagerapi.Workers.CardWorker;
+import com.example.conceptsworkmanagerapi.Workers.CleanupWorker;
+import com.example.conceptsworkmanagerapi.Workers.SaveCardToFileWorker;
+
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.work.Constraints;
+import androidx.work.Data;
 import androidx.work.ExistingWorkPolicy;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkContinuation;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
+import static com.example.conceptsworkmanagerapi.Constants.CUSTOM_QUOTE;
 import static com.example.conceptsworkmanagerapi.Constants.IMAGE_PROCESSING_WORK_NAME;
+import static com.example.conceptsworkmanagerapi.Constants.KEY_IMAGE_URI;
 import static com.example.conceptsworkmanagerapi.Constants.TAG_OUTPUT;
 
 public class CustomCardViewModel extends AndroidViewModel {
@@ -50,9 +58,38 @@ public class CustomCardViewModel extends AndroidViewModel {
 
     void processImageToCard(String quote) {
 
-        WorkContinuation workContinuation= workManager
+        WorkContinuation workContinuation = workManager
                 .beginUniqueWork(IMAGE_PROCESSING_WORK_NAME, ExistingWorkPolicy.REPLACE,
                         OneTimeWorkRequest.from(CleanupWorker.class));
+
+        OneTimeWorkRequest.Builder cardBuilder =
+                new OneTimeWorkRequest.Builder(CardWorker.class);
+        cardBuilder.setInputData(createInputDataForUri(quote));
+        workContinuation = workContinuation.then(cardBuilder.build());
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .build();
+
+        OneTimeWorkRequest save = new OneTimeWorkRequest.Builder(SaveCardToFileWorker.class)
+                .setConstraints(constraints)
+                .addTag(TAG_OUTPUT)
+                .build();
+
+        workContinuation = workContinuation.then(save);
+
+        workContinuation.enqueue();
+    }
+
+    private Data createInputDataForUri(String quote) {
+
+        Data.Builder dataBuilder = new Data.Builder();
+        if (!TextUtils.isEmpty(quote)) {
+            dataBuilder.putString(KEY_IMAGE_URI, imageUri.toString());
+            dataBuilder.putString(CUSTOM_QUOTE, quote);
+            return dataBuilder.build();
+        }
+        return null;
     }
 
     private Uri uriOrNull(String uriString) {
@@ -60,5 +97,13 @@ public class CustomCardViewModel extends AndroidViewModel {
             return Uri.parse(uriString);
         }
         return null;
+    }
+
+    public Uri getOutUri() {
+        return outputUri;
+    }
+
+    public void cancelWork() {
+
     }
 }
